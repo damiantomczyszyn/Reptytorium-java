0b9ece40447b222f10d4e5e1a414b00229e531b0
Damian Tomczyszyn <damitom@o2.pl>
2022-12-10 15:37:13 +0100

    Testowanie dzialania UDP, nastepny commit to juz zrobione

--- serverUDP/src/Server.java
+++ serverUDP/src/Server.java
@@ -8,48 +8,66 @@ public class Server extends Thread {
     private DatagramSocket socket;
     private boolean running;
     private byte[] buf = new byte[256];
-    String name = null;
-    String concat = "";
 
     public Server() throws SocketException {
-        socket = new DatagramSocket(4445);
-        concat = "";
+        socket = new DatagramSocket(4445); // Java provides DatagramSocket to communicate over UDP
     }
+    private StringBuilder data(byte[] a)
+    {
+        if (a == null)
+            return null;
+        StringBuilder ret = new StringBuilder();
+        int i = 0;
+        while (a[i] != 0)
+        {
+            ret.append((char) a[i]);
+            i++;
+        }
+        return ret;
+    }
+
 
     public void run() {
         running = true;
+        var count = 0;
 
         while (running) {
-            DatagramPacket packet
-                    = new DatagramPacket(buf, buf.length);
+            byte[] buf = new byte[256];
+            count++;
+            DatagramPacket packet = new DatagramPacket(buf, buf.length);
             try {
-                socket.receive(packet);
+                socket.receive(packet);// zapisane do bufora , a packet nie ma znaczenia
+                        //buf = packet.getData()
             } catch (IOException e) {
-                throw new RuntimeException(e);
+                e.printStackTrace();
             }
 
-            InetAddress address = packet.getAddress();
-            int port = packet.getPort();
+            InetAddress address = packet.getAddress();// wez adres z tego co przyszlo
+            int port = packet.getPort(); // wez port z tego co przyszlo
+
             packet = new DatagramPacket(buf, buf.length, address, port);
+            System.out.println(packet.getData());
+            String received = new String(packet.getData(), 0, packet.getLength());// czym jest offset
+            StringBuilder received2 = data(packet.getData());// czym jest offset
+            System.out.print(received);
+            System.out.print(".");
+            System.out.println();
+            System.out.print(received2.toString());System.out.println(".");
 
 
-            String received = new String(packet.getData(), 0, packet.getLength());
-            if (name == null)
-                name=received;
-            else concat+=received;
-
-            if (received.equals("end")) {
+            if (received.equals("end") || received.equals("") || count == 3){
                 running = false;
+                System.out.println("warunek");
                 continue;
+            //buf = String.getBytes(); na bajty do wysłania
             }
-            buf = concat.getBytes();
-            packet = new DatagramPacket(buf, buf.length , address, port);
             try {
                 socket.send(packet);
             } catch (IOException e) {
-                throw new RuntimeException(e);
+                e.printStackTrace();
             }
         }
+        System.out.println("zamykam polaczenie");
         socket.close();
     }
     public static void main(String[] args) throws SocketException {
